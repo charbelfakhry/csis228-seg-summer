3c87837c9e49e612a7895f6fb5eed07e0647a4df
chabelfakhry <charbel.b.fakhry@gmail.com>
2025-07-18 10:59:39 +0300

    views ejs

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 const AppError = require('./utils/AppError');
 require('dotenv').config();
+const path = require('path');
+const { title } = require('process');
+const UserService = require('./services/UserService');
 
 const app = express();
 
@@ -10,6 +13,16 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/dashboard', async (req, res) =>{
+    var userService = new UserService();
+    let users = await userService.getAll();
+    res.render('dashboard', {title: 'User Dashboard', users: users});
+});
 
 // Handle 404 errors
 app.all("/*all", (req, res,next) => {
